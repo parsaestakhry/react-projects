6e2e4e10c6809a8ec83490fd9e1872069107bf76
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-02-18 23:29:04 +0330

    finished setUp

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,9 +1,29 @@
+import { Brand } from "./components/brand/Brand";
+import { NavBar } from "./components/navbar/NavBar";
+import { Blog } from "./containers/blog/Blog";
+import { Features } from "./containers/features/Features";
 import { Footer } from "./containers/footer/Footer";
+import { Header } from "./containers/header/Header";
+import { Possibility } from "./containers/possibility/Possibility";
+import { WhatGPT3 } from "./containers/whatGPT3/WhatGPT3";
+import {Cta} from "./components/cta/Cta"
 
 function App() {
-  return <div>
-    <Footer/>
-  </div>;
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <NavBar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
--- project1/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Article } from "./Article/Article"
-export { default as Brand } from "./brand/Brand";
-export { default as Cta } from "./cta/Cta";
-export { default as Feature } from "./feature/Feature";
-export { default as NavBar } from "./navbar/NavBar";
