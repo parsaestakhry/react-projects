e90ec790f49a4da025101e2cdfd62c7bed1e4332
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-02-18 23:06:45 +0330

    import commit

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,6 +1,13 @@
+import { Article } from "./components/Article/Article";
+import {Brand} from "./components/brand/Brand"
+import {Cta} from "./components/cta/Cta"
+import {Feature} from "./components/feature/Feature"
+import {NavBar} from "./components/navbar/NavBar"
+
+
 function App() {
   return <div>
-    Hello
+    
   </div>;
 }
 
